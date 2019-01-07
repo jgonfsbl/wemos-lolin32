ce617ddf4428a2e56af30bfca994988fc91eda30
Jonathan <j@0x30.io>
2019-01-07 10:08:27 +0100

    Update boot.py

--- boot.py
+++ boot.py
@@ -1,34 +1,29 @@
 # This file is executed on every boot (including wake-boot from deepsleep)
-# EA1HET @ 2018-12-27
+# EA1HET @ 2019-01-01
 
-import gc
 import config
 import network
 import ubinascii as binascii
+from time import sleep
 
 
-print('\n[Booting] ')
-
+print('\n[Booting]')
 print('\nShutting down self-run AP')
 wlan_ap = network.WLAN(network.AP_IF)
 wlan_ap.active(False)    
 
 print('\nConnecting to IoT network')
-wlan_client = network.WLAN(network.STA_IF)
+wlan_client = network.WLAN(network.STA_IF) 
 
-if not wlan_client.isconnected():
-    wlan_client.active(True)
-    wlan_client.connect(config.essid, config.passphrase)
-
-    while not wlan_client.isconnected():
-        pass
-
-print('Connected to network')
-
-(ip, netmask, gateway, dns) = wlan_client.ifconfig()
-name = config.hostname + "-esp-%s" % binascii.hexlify(wlan_client.config("mac")[-3:]).decode("ascii")
-macaddress = binascii.hexlify(wlan_client.config('mac'),':').decode('utf-8') 
-
-gc.collect()
-
-
+for i in range(3):
+    if not wlan_client.isconnected():
+        wlan_client.active(True) 
+        wlan_client.connect(config.essid, config.passphrase)
+        sleep(5)
+    else:
+    	break
+        
+if wlan_client.isconnected(): 
+    (ip, netmask, gateway, dns) = wlan_client.ifconfig()  
+else:
+    wlan_client.active(False)    
